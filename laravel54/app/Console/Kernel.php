d84c73bc0e144cfda36af7f1ca8c568bc9b9eb8a
gengbinghan <977740256@qq.com>
2018-08-23 16:01:26 +0800

    elasticsearch配置

--- laravel54/app/Console/Kernel.php
+++ laravel54/app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\ESInit::class
     ];
 
     /**
@@ -37,4 +37,5 @@ class Kernel extends ConsoleKernel
     {
         require base_path('routes/console.php');
     }
+
 }
